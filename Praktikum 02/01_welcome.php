9ccfb20f5fb2676079b9b3accf0e90161939b609
mitslina <100134008+mitslina@users.noreply.github.com>
2024-09-03 00:08:04 +0700

    Add files via upload

---
+++ Praktikum 02/01_welcome.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+    
+    <body>
+        <?php
+        echo "<h2>Selamat datang di Praktikum PWI</h2><br/>";
+        echo "Hari ini praktikum : \"Sintaks Dasar PHP\"";
+        echo "Nama : \"nama_praktikan\"";
+        echo "NIM : \"nim_praktikan\"";
+        ?>
+    </body>
+</html>
---
+++ Praktikum 02/tugas.php
@@ -0,0 +1,22 @@
+<?php
+    // Nama : 
+    // NIM  : 
+
+    function hitung_rata($array) {
+        // TODO hitung rata-rata nilai mahasiswa
+    }
+
+    function print_mhs($array_mhs) {
+        // TODO tampilkan tabel
+    }
+
+    // Array data mahasiswa
+    $array_mhs = [
+        'Abdul' => [89, 90, 54],
+        'Budi' => [78, 60, 64],
+        'Nina' => [67, 56, 84],
+    ];
+
+    // Menampilkan data mahasiswa dalam bentuk tabel
+    print_mhs($array_mhs);
+?>
